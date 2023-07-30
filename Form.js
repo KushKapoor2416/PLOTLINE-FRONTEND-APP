ab05a7f5b663f960a01e1b46824459c084145801
kush kapoor <91105941+KushKapoor2416@users.noreply.github.com>
2023-07-30 15:45:33 +0530

    Update Form.js

--- Form.js
+++ Form.js
@@ -17,12 +17,21 @@ const Form = ({
     <div className="form-container" style={{ margin: "20px 30px", width: "643px", height: "612px", border: "1px solid #00000026", padding: "20px" }}>
       <form>
         <div className="form-group">
-          <label htmlFor="targetElement">Target Element:</label>
+          <label htmlFor="targetElement">Target Element:</label><br />
           <select
             id="targetElement"
             name="targetElement"
             value={targetElement}
             onChange={(e) => handleInputChange("targetElement", e.target.value)}
+            style={{
+              width: "547.56px",
+              height: "32px",
+              padding: "0px 12px 0px 12px",
+              borderRadius: "4px",
+              border: "1px solid",
+              gap: "4px",
+              background: "linear-gradient(0deg, #FFFFFF, #FFFFFF), linear-gradient(0deg, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15))",
+            }}
           >
             <option value="button1">Button 1</option>
             <option value="button2">Button 2</option>
@@ -40,6 +49,17 @@ const Form = ({
             name="tooltipText"
             value={tooltipText}
             onChange={(e) => handleInputChange("tooltipText", e.target.value)}
+            style={{
+              width: "547.56px",
+              height: "39px",
+              top: "-208px",
+              left: "-482.755126953125px",
+              padding: "0px 12px 0px 12px",
+              borderRadius: "6px",
+              border: "1px solid #00000026",
+              gap: "10px",
+              background: "linear-gradient(0deg, #FFFFFF, #FFFFFF), linear-gradient(0deg, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15))",
+            }}
           />
         </div>
         <div className="form-group">
@@ -47,35 +67,72 @@ const Form = ({
             Text Size:
           </label>
           <input
-            style={{ display: "inline-block", width: "100px" }}
-            type="text"
-            id="textSize"
-            name="textSize"
-            value={textSize}
-            onChange={(e) => handleInputChange("textSize", e.target.value)}
-          />
+  style={{
+    display: "inline-block",
+    width: "236.22px",
+    height: "39px",
+    top: "-131px",
+    left: "-482.755126953125px",
+    padding: "0px 0px 0px 12px",
+    borderRadius: "6px",
+    border: "1px solid #00000026",
+    gap: "12px",
+    background: "linear-gradient(0deg, #FFFFFF, #FFFFFF), linear-gradient(0deg, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15))",
+  }}
+  type="text"
+  id="textSize"
+  name="textSize"
+  value={textSize}
+  onChange={(e) => handleInputChange("textSize", e.target.value)}
+/>
+
           <label style={{ display: "block" }} htmlFor="padding">
             Padding:
           </label>
           <input
-            style={{ display: "inline-block", width: "100px" }}
-            type="text"
-            id="padding"
-            name="padding"
-            value={padding}
-            onChange={(e) => handleInputChange("padding", e.target.value)}
-          />
+  style={{
+    display: "inline-block",
+    width: "236.22px",
+    height: "39px",
+    top: "-131px",
+    left: "-179.90325927734375px",
+    padding: "0px 0px 0px 12px",
+    borderRadius: "6px",
+    border: "1px solid #00000026",
+    gap: "12px",
+    background: "linear-gradient(0deg, #FFFFFF, #FFFFFF), linear-gradient(0deg, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15))",
+  }}
+  type="text"
+  id="padding"
+  name="padding"
+  value={padding}
+  onChange={(e) => handleInputChange("padding", e.target.value)}
+/>
+
         </div>
         <div className="form-group">
           <label htmlFor="textColor">Text Color:</label>
           <br />
           <input
-            type="color"
-            id="textColor"
-            name="textColor"
-            value={textColor}
-            onChange={(e) => handleInputChange("textColor", e.target.value)}
-          />
+  type="color"
+  id="textColor"
+  name="textColor"
+  value={textColor}
+  onChange={(e) => handleInputChange("textColor", e.target.value)}
+  style={{
+    display: "inline-block",
+    width: "236.22px",
+    height: "39px",
+    top: "-131px",
+    left: "-179.90325927734375px",
+    padding: "0px 0px 0px 12px",
+    borderRadius: "6px",
+    border: "1px solid #00000026",
+    gap: "12px",
+    background: "linear-gradient(0deg, #FFFFFF, #FFFFFF), linear-gradient(0deg, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15))",
+  }}
+/>
+
         </div>
         <div className="form-group">
           <label htmlFor="backgroundColor">Background Color:</label>
